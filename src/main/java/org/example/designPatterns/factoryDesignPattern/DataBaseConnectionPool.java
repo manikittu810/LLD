e3c54602694e582a21a0b2f2e1691da277395e40
manikittu810 <msanganabatla@csu.fullerton.edu>
2025-01-30 21:22:40 -0800

    Factory pattern - DBConnectionPool Example V2(easy , eager initialization)

--- src/main/java/org/example/designPatterns/factoryDesignPattern/DataBaseConnectionPool.java
+++ src/main/java/org/example/designPatterns/factoryDesignPattern/DataBaseConnectionPool.java
@@ -2,9 +2,9 @@ package org.example.designPatterns.factoryDesignPattern;
 
 import java.util.Map;
 import java.util.function.Supplier;
-enum DBType{
-    MYSQL,MONGODB,POSTGRES
-}
+//enum DBType{
+//    MYSQL,MONGODB,POSTGRES
+//}
 public interface DataBaseConnectionPool {
     void useDB();
 }
@@ -27,22 +27,32 @@ class MongoDB implements DataBaseConnectionPool{
     }
 }
 class DBFactory{
-   private static final Map<DBType, Supplier<DataBaseConnectionPool>> dbServices = Map.of(
-           DBType.MYSQL,MySQL::new,
-           DBType.MONGODB,MongoDB::new,
-           DBType.POSTGRES,PostgreSQL::new
-   );
-   public static DataBaseConnectionPool createDBConnection(DBType type){
-       return dbServices.getOrDefault(type,
-               ()-> {
-           throw new IllegalArgumentException("Invalid DB option");
-       }
-       ).get();
-   }
+//   private static final Map<DBType, Supplier<DataBaseConnectionPool>> dbServices = Map.of(
+//           DBType.MYSQL,MySQL::new,
+//           DBType.MONGODB,MongoDB::new,
+//           DBType.POSTGRES,PostgreSQL::new
+//   );
+//   public static DataBaseConnectionPool createDBConnection(DBType type){
+//       return dbServices.getOrDefault(type,
+//               ()-> {
+//           throw new IllegalArgumentException("Invalid DB option");
+//       }
+//       ).get();
+//   }
+    public static DataBaseConnectionPool createDBConnection(int connect){
+        return switch(connect){
+            case 1 -> new MySQL();
+            case 2 -> new MongoDB();
+            case 3-> new PostgreSQL();
+            default -> throw new IllegalArgumentException("Invalid DB connection request....");
+        };
+    }
 }
 class TestDBConnectionPool{
     public static void main(String[] args) {
-        DataBaseConnectionPool db = DBFactory.createDBConnection(DBType.MYSQL);
+//        DataBaseConnectionPool db = DBFactory.createDBConnection(DBType.MYSQL);
+        DataBaseConnectionPool db = DBFactory.createDBConnection(3);
+
         db.useDB();
     }
 }

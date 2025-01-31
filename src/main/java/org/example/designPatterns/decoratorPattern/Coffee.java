7e7a451c5c46fef9ffa0265d22b32d79a1a78bc6
manikittu810 <msanganabatla@csu.fullerton.edu>
2025-01-30 20:11:16 -0800

    Decorator pattern - Coffee Shop - some more decorators added

--- src/main/java/org/example/designPatterns/decoratorPattern/Coffee.java
+++ src/main/java/org/example/designPatterns/decoratorPattern/Coffee.java
@@ -1,10 +1,11 @@
 package org.example.designPatterns.decoratorPattern;
 
+import java.util.Scanner;
+
 public interface Coffee {
     String getDescription();
     double getCost();
 }
-
 class BasicCoffee implements Coffee{
     private  final String description;
     private  final double cost;
@@ -51,6 +52,19 @@ class MilkDecorator extends CoffeeDecorator{
         return coffee.getCost() + 1.50;
     }
 }
+class OatMilkDecorator extends CoffeeDecorator{
+    public OatMilkDecorator(Coffee coffee){
+        super(coffee);
+    }
+    @Override
+    public String getDescription(){
+        return coffee.getDescription() +", with added oat milk";
+    }
+    @Override
+    public double getCost(){
+        return coffee.getCost() + 1.70;
+    }
+}
 class SugarDecorator extends CoffeeDecorator{
     public SugarDecorator(Coffee coffee){
         super(coffee);
@@ -77,23 +91,68 @@ class WhippedCreamDecorator extends CoffeeDecorator{
         return coffee.getCost() + 1.75;
     }
 }
+class BlueBerryDecorator extends CoffeeDecorator{
+    public BlueBerryDecorator(Coffee coffee){
+        super(coffee);
+    }
+    @Override
+    public String getDescription(){
+        return coffee.getDescription() +", with added Blue berry";
+    }
+    @Override
+    public double getCost(){
+        return coffee.getCost() + 1.25;
+    }
+}
+class CaramelSyrupDecorator extends CoffeeDecorator{
+    public CaramelSyrupDecorator(Coffee coffee){
+        super(coffee);
+    }
+    @Override
+    public String getDescription(){
+        return coffee.getDescription() +", with added Caramel Syrup";
+    }
+    @Override
+    public double getCost(){
+        return coffee.getCost() + 1.85;
+    }
+}
 class TestDecorator{
     public static void main(String[] args) {
         System.out.println("Welcome to SMK Coffee Dhukaanam :) ");
-        Coffee coffee = new BasicCoffee("Pike Roast",3.25);
+
+        Scanner sc = new Scanner(System.in);
+
+        String coffeeName = sc.next();
+
+        Coffee coffee = new BasicCoffee(coffeeName,3.25);
         System.out.println("Coffee Type : "+coffee.getDescription());
         System.out.println("Cost : $" + coffee.getCost());
 
-        Coffee milkDecorator = new MilkDecorator(coffee);
-        System.out.println("Coffee Type : "+milkDecorator.getDescription());
-        System.out.println("Cost : $" + milkDecorator.getCost());
+//        Coffee milkDecorator = new MilkDecorator(coffee);
+//        System.out.println("Coffee Type : "+milkDecorator.getDescription());
+//        System.out.println("Cost : $" + milkDecorator.getCost());
 
-        Coffee sugarCoffee = new SugarDecorator(milkDecorator);
-        System.out.println("Coffee Type : "+sugarCoffee.getDescription());
-        System.out.println("Cost : $" + sugarCoffee.getCost());
+        Coffee oatMilkDecorator = new OatMilkDecorator(coffee);
+        System.out.println("Coffee Type : "+oatMilkDecorator.getDescription());
+        System.out.println("Cost : $" + oatMilkDecorator.getCost());
 
-        Coffee whipped = new WhippedCreamDecorator(sugarCoffee);
+//        Coffee sugarCoffee = new SugarDecorator(milkDecorator);
+//        System.out.println("Coffee Type : "+sugarCoffee.getDescription());
+//        System.out.println("Cost : $" + sugarCoffee.getCost());
+
+        Coffee whipped = new WhippedCreamDecorator(oatMilkDecorator);
         System.out.println("Coffee Type : "+whipped.getDescription());
         System.out.println("Cost : $" + whipped.getCost());
+
+        Coffee blueBerryAddon = new BlueBerryDecorator(whipped);
+        System.out.println("Coffee Type : "+blueBerryAddon.getDescription());
+        System.out.println("Cost : $" + blueBerryAddon.getCost());
+
+        Coffee caramelSyrup = new CaramelSyrupDecorator(blueBerryAddon);
+        System.out.println("Coffee Type : "+caramelSyrup.getDescription());
+        System.out.println("Cost : $" + caramelSyrup.getCost());
+
     }
 }
+//milk type -oat milk, base -> macha - base , wrapper -> blue berry , caramel syrup
